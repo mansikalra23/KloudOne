598ec7c9508950d3b366bc1d7a5090b754316390
Mansi Kalra <54201347+mansikalra23@users.noreply.github.com>
2020-11-19 18:32:17 +0530

    Add files via upload

--- QuickSort.go
+++ QuickSort.go
@@ -73,3 +73,52 @@ func merge(left, right []int) (result []int) {
 
 	return
 }
+package main
+
+import (
+	"fmt"
+)
+
+func quickSort(arr []int, low, high int) {
+	if low < high {
+		var piv = partition(arr, low, high)
+		quickSort(arr, low, piv)
+		quickSort(arr, piv+1, high)
+	}
+}
+
+func partition(arr []int, low, high int) int {
+	var piv = arr[low]
+	var i = low
+	var j = high
+
+	for i < j {
+		for arr[i] <= piv && i < high {
+			i++
+		}
+		for arr[j] > piv && j > low {
+			j--
+		}
+
+		if i < j {
+			var temp = arr[i]
+			arr[i] = arr[j]
+			arr[j] = temp
+		}
+	}
+
+	arr[low] = arr[j]
+	arr[j] = piv
+
+	return j
+}
+
+func main() {
+
+	var arr = []int{15, 3, 12, 6, -9, 9, 0}
+
+	fmt.Println("Unsorted : ", arr)
+
+	quickSort(arr, 0, len(arr)-1)
+	fmt.Println("Sorted : ", arr)
+}
